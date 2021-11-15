b7e74f8f5b04f6b0adb9135878325a19ab5ed6b4
Akanowo <ukoakanowo98@gmail.com>
2021-11-15 22:41:01 +0100

    changed mongo uri

--- configs/dbConfig.js
+++ configs/dbConfig.js
@@ -2,15 +2,10 @@ const mongoose = require('mongoose');
 
 function dbConfig() {
 	mongoose
-		.connect(
-			process.env.NODE_ENV && process.env.NODE_ENV.trim() === 'production'
-				? process.env.MONGO_URI
-				: 'mongodb://localhost:27017/EthanMediaDB',
-			{
-				useUnifiedTopology: true,
-				useNewUrlParser: true,
-			}
-		)
+		.connect(process.env.MONGO_URI, {
+			useUnifiedTopology: true,
+			useNewUrlParser: true,
+		})
 		.then((client) => {
 			if (client) {
 				console.log('DB connected sucessfully'.blue.bold);
